58b81d9478a6831fb22f0e933aa705cb6f391aa7
JDevMen <jdmendoza_90@hotmail.com>
2021-09-27 14:48:02 -0500

    Escena de prueba, prefabs de prueba y scripts de prueba nuevo generador

--- Assets/Scripts/SpawnGameObjectTest.cs
+++ Assets/Scripts/SpawnGameObjectTest.cs
@@ -2,62 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnGameObject : MonoBehaviour
+public class SpawnGameObjectTest : MonoBehaviour
 {
     public GameObject pelotaPrefab;
+    public GameObject reticle;
 
     public float minSecondsBetweenSpawning = 3.0f;
     public float maxSecondsBetweenSpawning = 6.0f;
 
     public float velocidadPelota = 5.0f;
+    public float tamanoPelota = 1.0f;
     public int bounce_min = 1;
     public int bounce_max = 6;
     public int player_bounces = 1;
 
     private float savedTime;
     private float secondsBetweenSpawning;
-    private GameObject pelotaCopy;
+    private Vector2 direction;
 
     // Use this for initialization
     void Start()
     {
         savedTime = Time.time;
         secondsBetweenSpawning = Random.Range(minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
-        //Instanciación inicial del prefab de pelota
-        pelotaCopy = Instantiate(pelotaPrefab) ;
-        pelotaCopy.GetComponent<pelota_script>().speed = velocidadPelota;
-        pelotaCopy.GetComponent<pelota_script>().bounce_min = bounce_min;
-        pelotaCopy.GetComponent<pelota_script>().bounce_max = bounce_max;
-        pelotaCopy.GetComponent<pelota_script>().player_bounces = player_bounces;
     }
 
     // Update is called once per frame
     void Update()
-    { 
+    {
+        Vector2 reticlePos = reticle.transform.position;
+
+        direction = reticlePos - (Vector2)transform.position;
+
         if (Time.time - savedTime >= secondsBetweenSpawning) // is it time to spawn again?
         {
             //Instanciación del prefab para generar una nueva pelota
-            pelotaCopy = Instantiate(pelotaPrefab);
-            pelotaCopy.GetComponent<pelota_script>().speed = velocidadPelota;
-            pelotaCopy.GetComponent<pelota_script>().bounce_min = bounce_min;
-            pelotaCopy.GetComponent<pelota_script>().bounce_max = bounce_max;
-            pelotaCopy.GetComponent<pelota_script>().player_bounces = player_bounces;
-            MakeThingToSpawn();
+            shootObject();
             savedTime = Time.time; // store for next spawn
             secondsBetweenSpawning = Random.Range(minSecondsBetweenSpawning, maxSecondsBetweenSpawning);
         }
     }
 
-    void MakeThingToSpawn()
-    {
-        
-        // create a new gameObject
-        GameObject clone = Instantiate(pelotaCopy, transform.position, transform.rotation) as GameObject;
-
-    }
-
-    void spawnObject()
+    void shootObject()
     {
         //Instanciación del prefab para generar una nueva pelota
+        GameObject pelotaCopy = Instantiate(pelotaPrefab, reticle.transform.position, Quaternion.identity);
+        pelotaCopy.GetComponent<pelota_test_script>().speed = velocidadPelota;
+
+        pelotaCopy.GetComponent<pelota_test_script>().bounce_min = bounce_min;
+        pelotaCopy.GetComponent<pelota_test_script>().bounce_max = bounce_max;
+        pelotaCopy.GetComponent<pelota_test_script>().player_bounces = player_bounces;
+        pelotaCopy.GetComponent<pelota_test_script>().direccion = direction;
     }
 }
