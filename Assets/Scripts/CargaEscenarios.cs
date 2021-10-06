7995f87865e0c8df3899c01a418efb7024022eb9
Julio Morales <ja.morales11@uniandes.edu.co>
2021-10-06 18:52:08 -0500

    Escenarios finales básicos + lógica puntaje base + prefabs basicos

---
+++ Assets/Scripts/CargaEscenarios.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CargaEscenarios : MonoBehaviour
+{
+
+    public GameObject esce1;
+    public GameObject esce2;
+    public GameObject esce3;
+    public GameObject esce4;
+
+    public void CargarMenu()
+    {
+        Debug.Log("click");
+        SceneManager.LoadScene(0);
+        
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int escenario = EvaluacionPuntaje.escenarioCargar;
+        Debug.Log(escenario);
+
+        if (escenario == 1)
+        {
+            esce1.SetActive(true);
+        }
+        else if (escenario == 2)
+        {
+            esce2.SetActive(true);
+        }
+        else if (escenario == 3)
+        {
+            esce3.SetActive(true);
+        }
+        else if (escenario == 4)
+        {
+            esce4.SetActive(true);
+        }
+
+
+    }
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    
+
+
+
+}
---
+++ Assets/Scripts/EvaluacionPuntaje.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EvaluacionPuntaje : MonoBehaviour
+{
+    private Temporizador temp;
+    private int tiempo;
+    public static int escenarioCargar;
+
+    private paddle_script jugadorScript;
+
+    //puntos del jugador
+    private float puntosEnergia;
+    private float puntosUniversidad;
+    private float puntosFamilia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject UIcanvas = GameObject.FindGameObjectWithTag("UIcanvas");
+
+        temp = UIcanvas.GetComponent<Temporizador>();
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            jugadorScript = player.GetComponent<paddle_script>();
+            Debug.Log("Se encontró y asignó script de control del jugador.");
+            Debug.Log("-------------------------------");
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        tiempo = temp.getTiempo();
+
+        //puntosEnergia = jugadorScript.getPuntosEnergia();
+        //puntosUniversidad = jugadorScript.getPuntosUniversidad();
+        //puntosFamilia = jugadorScript.getPuntosFamilia();
+
+        if (tiempo == 0)
+        {
+            
+        //    if(puntosUniversidad > puntosFamilia + 5 && puntosUniversidad > puntosEnergia + 5 )
+        //    {
+        //        escenarioCargar = 1;
+        //    }
+        //    else if (puntosFamilia > puntosUniversidad + 5 && puntosFamilia > puntosEnergia + 5)
+        //    {
+        //        escenarioCargar = 2;
+        //    }
+        //    else if (puntosEnergia > puntosUniversidad + 5 && puntosEnergia > puntosFamilia + 5)
+        //    {
+        //        escenarioCargar = 3;
+        //    }
+        //    else
+        //    {
+        //        escenarioCargar = 4;
+        //    }
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    
+
+
+}
