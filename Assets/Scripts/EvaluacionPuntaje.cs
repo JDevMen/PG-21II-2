c15a6425b0e90327a89865b91020b395891a5687
JDevMen <jdmendoza_90@hotmail.com>
2021-10-07 11:02:46 -0500

    ajustes de escalamiento

--- Assets/Scripts/EvaluacionPuntaje.cs
+++ Assets/Scripts/EvaluacionPuntaje.cs
@@ -36,32 +36,34 @@ public class EvaluacionPuntaje : MonoBehaviour
         void Update()
     {
         tiempo = temp.getTiempo();
-
-        puntosEnergia = jugadorScript.getPuntosEnergia();
-        puntosUniversidad = jugadorScript.getPuntosUniversidad();
-        puntosFamilia = jugadorScript.getPuntosFamilia();
-
-        if (tiempo == 0)
+        if (jugadorScript != null)
         {
+            puntosEnergia = jugadorScript.getPuntosEnergia();
+            puntosUniversidad = jugadorScript.getPuntosUniversidad();
+            puntosFamilia = jugadorScript.getPuntosFamilia();
 
-            if (puntosUniversidad > puntosFamilia + 5 && puntosUniversidad > puntosEnergia + 5)
+            if (tiempo == 0)
             {
-                escenarioCargar = 1;
-            }
-            else if (puntosFamilia > puntosUniversidad + 5 && puntosFamilia > puntosEnergia + 5)
-            {
-                escenarioCargar = 2;
-            }
-            else if (puntosEnergia > puntosUniversidad + 5 && puntosEnergia > puntosFamilia + 5)
-            {
-                escenarioCargar = 3;
-            }
-            else
-            {
-                escenarioCargar = 4;
-            }
 
-            SceneManager.LoadScene("EscenariosFinal");
+                if (puntosUniversidad > puntosFamilia + 5 && puntosUniversidad > puntosEnergia + 5)
+                {
+                    escenarioCargar = 1;
+                }
+                else if (puntosFamilia > puntosUniversidad + 5 && puntosFamilia > puntosEnergia + 5)
+                {
+                    escenarioCargar = 2;
+                }
+                else if (puntosEnergia > puntosUniversidad + 5 && puntosEnergia > puntosFamilia + 5)
+                {
+                    escenarioCargar = 3;
+                }
+                else
+                {
+                    escenarioCargar = 4;
+                }
+
+                SceneManager.LoadScene("EscenariosFinal");
+            }
         }
     }
 
