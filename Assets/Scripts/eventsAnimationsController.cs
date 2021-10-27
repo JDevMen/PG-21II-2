711bac81a0a3ef0fadf82cff5b6198b5858447b1
JDevMen <jdmendoza_90@hotmail.com>
2021-10-26 23:00:38 -0500

    Agregados todos los spritres, cambio de usar objetos normales por images de UI para mejor consistencia en UI.

--- Assets/Scripts/eventsAnimationsController.cs
+++ Assets/Scripts/eventsAnimationsController.cs
@@ -7,70 +7,59 @@ public class eventsAnimationsController : MonoBehaviour
     public GameObject pelotaEventUI;
     public GameObject jugadorEventUI;
 
-    //Prueba cambio sprite en tiempo de ejecución
-    public Sprite pelotaSprite1;
-    public Sprite pelotaSprite2;
-
-    //Sprites de eventos
-    private Sprite debuffTamanoPelota;
-
-
     private Animator pelotaAnimator;
     private Animator jugadorAnimator;
-    private SpriteRenderer pelotaEventRenderer;
-    private SpriteRenderer jugadorEventRenderer;
+
+    //Lista de sprites de todos los iconos,
+    //iconos predeterminados en pos 0 y 1
+    private Sprite[] icons;
 
 
     private void Start()
     {
         pelotaAnimator = pelotaEventUI.GetComponent<Animator>();
         jugadorAnimator = jugadorEventUI.GetComponent<Animator>();
-        pelotaEventRenderer = pelotaEventUI.GetComponent<SpriteRenderer>();
-        jugadorEventRenderer = jugadorEventUI.GetComponent<SpriteRenderer>();
+
+        loadIcons();
+
+        Debug.Log(icons[0]);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Código para probar.
         if (Input.GetKey(KeyCode.F1))
         {
-            StartCoroutine(pelotaAnimationCoroutine());
-            StartCoroutine(jugadorAnimationCoroutine());
+            pelotaAnimator.Play("lookAtMe");
         }
-        if (Input.GetKey(KeyCode.F2))
-        {
-            pelotaEventUI.SetActive(true);
-            pelotaAnimator.Play("Default");
-            jugadorEventUI.SetActive(true);
-            jugadorAnimator.Play("Default");
-        }
-        if (Input.GetKey(KeyCode.F3))
-        {
-            Debug.Log("F3 pressed");
-            pelotaEventRenderer.sprite = pelotaSprite1;
-        }
-        if (Input.GetKey(KeyCode.F4))
-        {
-            Debug.Log("F4 pressed");
-            pelotaEventRenderer.sprite = pelotaSprite2;
-        }
-
 
     }
 
-    public IEnumerator pelotaAnimationCoroutine()
+    public IEnumerator pelotaAnimationCoroutine(float pDuracion)
     {
         pelotaAnimator.Play("EventoPelotaAnimation");
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(pDuracion);
         pelotaAnimator.Play("Default");
-        pelotaEventUI.SetActive(false);
     }
-    public IEnumerator jugadorAnimationCoroutine()
+    public IEnumerator jugadorAnimationCoroutine(float pDuracion)
     {
         jugadorAnimator.Play("EventoJugadorAnimation");
-        yield return new WaitForSeconds(3f);
+        yield return new WaitForSeconds(pDuracion);
         jugadorAnimator.Play("Default");
-        jugadorEventUI.SetActive(false);
     }
+
+    void loadIcons()
+    {
+        object[] loadedIcons = Resources.LoadAll<Sprite>("EventIcons");
+
+        icons = new Sprite[loadedIcons.Length];
+
+        for(int i = 0; i<icons.Length;i++)
+        {
+            icons[i] = (Sprite)loadedIcons[i];
+        }
+
+    }
+
 }
