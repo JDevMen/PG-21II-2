e832bab003645b8bb552d7b91019f1cc85047394
JDevMen <jdmendoza_90@hotmail.com>
2021-11-17 23:12:01 -0500

    Cambio a varios scripts para tener variable tiempo como float yt además corutina para cambiar por semanas

--- Assets/Scripts/Temporizador.cs
+++ Assets/Scripts/Temporizador.cs
@@ -6,18 +6,24 @@ using UnityEngine.UI;
 
 public class Temporizador : MonoBehaviour
 {
-    private int tiempo = 60;
+    private float tiempo = 60;
     public Text tiempoDisplay;
     public Slider tiempoDisplay2;
     public GameObject menuPausa;
-    public int tiempoParaCalcular;
+    public float tiempoParaCalcular;
+
+    private int numeroSemana = 0;
 
 
     private void Start()
     {
+        float tiempoPorSemanas = tiempo / 16;
         Contador();
+        StartCoroutine(ContadorSemanas(tiempoPorSemanas));
         tiempoDisplay2.maxValue = tiempo;
         tiempoParaCalcular = tiempo;
+
+        Debug.Log("Tiempo por semana " + tiempoPorSemanas);
     }
 
     private void Update()
@@ -26,24 +32,36 @@ public class Temporizador : MonoBehaviour
 
     }
 
-    public int getTiempo()
+    public float getTiempo()
     {
         return tiempo;
     }
+
+    IEnumerator ContadorSemanas(float pTiempoPorSemana) {
+
+        while (tiempo>0)
+        {
+            numeroSemana++;
+            tiempoDisplay.text = "Semana " + numeroSemana;
+
+             yield return new WaitForSeconds(pTiempoPorSemana);
+        }
+    }
+
 
     void Contador()
     {
         if (tiempo > 0 && menuPausa.activeSelf == false)
         {
-            TimeSpan spanTime = TimeSpan.FromSeconds(tiempo);
-            if(spanTime.Seconds < 10)
-            {
-                tiempoDisplay.text = "0" + spanTime.Minutes + ":0" + spanTime.Seconds;
-            }
-            else
-            {
-                tiempoDisplay.text = "0" + spanTime.Minutes + ":" + spanTime.Seconds;
-            }
+            //TimeSpan spanTime = TimeSpan.FromSeconds(tiempo);
+            //if(spanTime.Seconds < 10)
+            //{
+            //    tiempoDisplay.text = "0" + spanTime.Minutes + ":0" + spanTime.Seconds;
+            //}
+            //else
+            //{
+            //    tiempoDisplay.text = "0" + spanTime.Minutes + ":" + spanTime.Seconds;
+            //}
             
             tiempo--;
             
